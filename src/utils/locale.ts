317990124007df80ee0362e83411743a35c18550
redleafnew <redleafnew@163.com>
2023-06-18 15:15:55 +0800

    升级locale

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,9 +1,11 @@
 import { config } from "../../package.json";
 
+export { initLocale, getString };
+
 /**
  * Initialize locale data
  */
-export function initLocale() {
+function initLocale() {
   const l10n = new (ztoolkit.getGlobal("Localization"))(
     [`${config.addonRef}-addon.ftl`],
     true
@@ -14,23 +16,55 @@ export function initLocale() {
 }
 
 /**
- * Get locale string
- * @param localString
- * @param branch branch name
+ * Get locale string, see https://firefox-source-docs.mozilla.org/l10n/fluent/tutorial.html#fluent-translation-list-ftl
+ * @param localString ftl key
+ * @param options.branch branch name
+ * @param options.args args
  * @example
  * ```ftl
  * # addon.ftl
- * addon-name = Addon Template
- *     .label = Addon Template Label
+ * addon-static-example = This is default branch!
+ *     .branch-example = This is a branch under addon-static-example!
+ * addon-dynamic-example =
+    { $count ->
+        [one] I have { $count } apple
+       *[other] I have { $count } apples
+    }
  * ```
  * ```js
- * getString("addon-name"); // Addon Template
- * getString("addon-name", "label"); // Addon Template Label
+ * getString("addon-static-example"); // This is default branch!
+ * getString("addon-static-example", { branch: "branch-example" }); // This is a branch under addon-static-example!
+ * getString("addon-dynamic-example", { args: { count: 1 } }); // I have 1 apple
+ * getString("addon-dynamic-example", { args: { count: 2 } }); // I have 2 apples
  * ```
  */
-export function getString(localString: string, branch = ""): string {
+function getString(localString: string): string;
+function getString(localString: string, branch: string): string;
+function getString(
+  localString: string,
+  options: { branch?: string | undefined; args?: Record<string, unknown> }
+): string;
+function getString(...inputs: any[]) {
+  if (inputs.length === 1) {
+    return _getString(inputs[0]);
+  } else if (inputs.length === 2) {
+    if (typeof inputs[1] === "string") {
+      return _getString(inputs[0], { branch: inputs[1] });
+    } else {
+      return _getString(inputs[0], inputs[1]);
+    }
+  } else {
+    throw new Error("Invalid arguments");
+  }
+}
+
+function _getString(
+  localString: string,
+  options: { branch?: string | undefined; args?: Record<string, unknown> } = {}
+): string {
+  const { branch, args } = options;
   const pattern = addon.data.locale?.current.formatMessagesSync([
-    { id: localString },
+    { id: localString, args },
   ])[0];
   if (!pattern) {
     return localString;
